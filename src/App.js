c1ceee5ec88078f982dc6a90c7cd6cd677b753e1
linda_forlizzi <la.for.lizzi@gmail.com>
2022-01-01 12:07:38 -0500

    complete except image is not responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import Header from './components/Header'
 import Meme from './components/Meme'
-import data from './memesData'
 
 export default function App() {
-  	// const memes = data.memes.map(meme => {
-	// 	return (
-	// 		<Meme
-	// 			key={meme.id}
-	// 			{...meme}
-	// 		/>
-	// 	)
-  	// })
 
 	return (
 		<div className="App">
 			<Header />
 			<Meme />
-			{/* <div className='meme--image'>
-				{memes}
-			</div> */}
 		</div>
 	)
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react'
-// import memesData from "../memesData"
 
 export default function Meme(props) {
     const [meme, setMeme] = useState({
